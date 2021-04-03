c6de5440153122e5154995db474e96e6468424ac
Tunghim Lam <tunghim@gmail.com>
2021-04-03 21:58:29 +0800

    + thread handling for macOS Shorten var names Untrack test.py

--- assets/index.js
+++ assets/index.js
@@ -1,11 +1,11 @@
 $("document").ready(function() {
   $('#recordBtn').click(function() {
-    var msg = 'Record btn is clicked.'
+    var msg = 'Record btn clicked'
     window.pywebview.api.record(msg)
   })
 
   $('#playBtn').click(function() {
-    var msg = 'Play btn is clicked.'
+    var msg = 'Play btn clicked'
     window.pywebview.api.play(msg)
   })
 })
--- main.py
+++ main.py
@@ -20,13 +20,13 @@ class Api:
         logger.info(msg)
 
         self.kb_events = []
-        self.mouse_events = []
+        self.m_events = []
 
         kb.start_recording()
-        m.hook(self.mouse_event_handler)
+        m.hook(self.m_event_handler)
         kb.wait(self.escape_key)
         raw_kb_events = kb.stop_recording()
-        m.unhook(self.mouse_event_handler)
+        m.unhook(self.m_event_handler)
         self.kb_event_handler(raw_kb_events)
 
         self.save()
@@ -34,45 +34,39 @@ class Api:
         logger.info('Record finished.')
 
     def play(self, msg, god_speed=False):
-        try:
-            logger.info(msg)
+        logger.info(msg)
 
-            events = self.load()
-            state = kb.stash_state()
+        events = self.load()
+        state = kb.stash_state()
 
-            # Hot fix for missing 1st event
-            kb.unhook(kb.hook(set().add))
+        last_time = None
+        for event in events:
+            logger.info(event)
 
-            last_time = None
-            for event in events:
-                logger.info(event)
+            if last_time and not god_speed:
+                time.sleep(event['time'] - last_time)
+            else:
+                time.sleep(0.05)
+            last_time = event['time']
 
-                if last_time and not god_speed:
-                    time.sleep(event['time'] - last_time)
+            # Action
+            if event['event_name'] == 'ButtonEvent':
+                if event['event_type'] == 'up':
+                    pag.mouseUp(
+                        button=event['button'], x=event['position'][0], y=event['position'][1])
                 else:
-                    time.sleep(0.05)
-                last_time = event['time']
-
-                # Action
-                if event['event_name'] == 'ButtonEvent':
-                    if event['event_type'] == 'up':
-                        pag.mouseUp(
-                            button=event['button'], x=event['position'][0], y=event['position'][1])
-                    else:
-                        pag.mouseDown(
-                            button=event['button'], x=event['position'][0], y=event['position'][1])
+                    pag.mouseDown(
+                        button=event['button'], x=event['position'][0], y=event['position'][1])
+            else:
+                key = event['scan_code'] or event['name']
+                if event['event_type'] == 'up':
+                    kb.release(key)
                 else:
-                    key = event['scan_code'] or event['name']
-                    if event['event_type'] == 'up':
-                        kb.release(key)
-                    else:
-                        kb.press(key)
+                    kb.press(key)
 
-            kb.restore_modifiers(state)
+        kb.restore_modifiers(state)
 
-            logger.info('Replay finished.')
-        except Exception as e:
-            logger.info(str(e))
+        logger.info('Replay finished.')
 
     def kb_event_handler(self, raw_kb_events):
         for e in raw_kb_events:
@@ -81,18 +75,18 @@ class Api:
             kb_event_dict.update(json.loads(e.to_json()))
             self.kb_events.append(kb_event_dict)
 
-    def mouse_event_handler(self, raw_mouse_event):
-        if isinstance(raw_mouse_event, m.ButtonEvent) and raw_mouse_event.button in ['?', 'left', 'middle', 'right']:
-            mouse_event_dict = {}
-            mouse_event_dict['event_name'] = 'ButtonEvent'
-            mouse_event_dict['event_type'] = raw_mouse_event.event_type
-            mouse_event_dict['button'] = 'left' if raw_mouse_event.button == '?' else raw_mouse_event.button
-            mouse_event_dict['position'] = m.get_position()
-            mouse_event_dict['time'] = raw_mouse_event.time
-            self.mouse_events.append(mouse_event_dict)
+    def m_event_handler(self, raw_m_event):
+        if isinstance(raw_m_event, m.ButtonEvent) and raw_m_event.button in ['?', 'left', 'middle', 'right']:
+            m_event_dict = {}
+            m_event_dict['event_name'] = 'ButtonEvent'
+            m_event_dict['event_type'] = raw_m_event.event_type
+            m_event_dict['button'] = 'left' if raw_m_event.button == '?' else raw_m_event.button
+            m_event_dict['position'] = m.get_position()
+            m_event_dict['time'] = raw_m_event.time
+            self.m_events.append(m_event_dict)
 
     def save(self):
-        events = sorted(self.kb_events + self.mouse_events,
+        events = sorted(self.kb_events + self.m_events,
                         key=lambda i: i['time'])
 
         # Fix escape_key pressing issue
@@ -104,16 +98,23 @@ class Api:
             'time': events[-1]['time'] + 0.1
         })
 
+        # To do: dynamic file name
         with open('events.json', 'w') as f:
             json.dump(events, f)
 
     def load(self):
+        # To do: dynamic file name
         with open('events.json') as f:
             events = json.load(f)
+
         return events
+
+    def thread_handler(self):
+        kb.unhook(kb.hook(set().add))
+        kb.release(self.escape_key)
 
 
 if __name__ == '__main__':
     api = Api()
     window = webview.create_window('Chrono', 'assets/index.html', js_api=api)
-    webview.start()
+    webview.start(api.thread_handler)
