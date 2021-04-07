8339eb8c2c8ca045108ff4d1e7de9054bc0771cb
Tunghim Lam <tunghim@cronumax.com>
2021-04-07 21:49:55 +0800

    Migrate to pynput for non-root access & system notification feasibility. Notification for record & replay finished.

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import webview
 import logging
-import keyboard as kb
-import mouse as m
 import time
 import json
-import pyautogui as pag
 import platform
+import subprocess as s
+from pynput.keyboard import Key, Controller as kb_ctrler, Listener as kb_lstner
+from pynput.mouse import Button, Controller as m_ctrler, Listener as m_lstner
+from pynput.keyboard._xorg import KeyCode as LinuxKeyCode
 
 
 logging.basicConfig(filename='Chrono.log', level=logging.DEBUG,
@@ -15,106 +16,169 @@ logger = logging.getLogger(__name__)
 
 class Api:
     def __init__(self):
-        self.escape_key = 'esc'  # To do: let user select
+        self.is_recording = False
+        self.is_playing = False
+        '''
+        To do: let user change these settings
+        '''
+        self.escape_key = Key.esc
+        self.touch_mode = True
+        self.god_speed = False
 
     def record(self, msg):
         logger.info(msg)
 
-        self.kb_events = []
-        self.m_events = []
+        if not self.is_recording:
+            self.is_recording = True
+            self.kb_events = []
+            self.m_events = []
 
-        kb.start_recording()
-        m.hook(self.m_event_handler)
-        kb.wait(self.escape_key)
-        raw_kb_events = kb.stop_recording()
-        m.unhook(self.m_event_handler)
-        self.kb_event_handler(raw_kb_events)
+            with kb_lstner(on_press=self.on_press, on_release=self.on_release) as self.kl, m_lstner(on_move=self.on_touch, on_click=self.on_click, on_scroll=self.on_scroll) as self.ml:
+                self.kl.join()
+                self.ml.join()
 
-        self.save()
+            # logger.info(self.kb_events)
+            # logger.info(self.m_events)
+            try:
+                self.save()
+            except Exception as e:
+                logger.error(str(e))
 
-        logger.info('Record finished')
+            logger.info('Record finished')
+            s.call(['notify-send', 'Chrono', 'Record finished.'])
+            self.is_recording = False
 
-    def play(self, msg, god_speed=False):
+    def play(self, msg):
         logger.info(msg)
 
-        events = self.load()
-        state = kb.stash_state()
+        if not self.is_playing:
+            self.is_playing = True
+            events = self.load()
+            kc = kb_ctrler()
+            mc = m_ctrler()
 
-        last_time = None
-        for event in events:
-            logger.info(event)
+            last_time = None
+            for event in events:
+                logger.info(event)
 
-            if last_time and not god_speed:
-                time.sleep(event['time'] - last_time)
-            else:
-                time.sleep(0.05)
-            last_time = event['time']
-
-            # Action
-            if event['event_name'] == 'ButtonEvent':
-                if event['event_type'] == 'up':
-                    pag.mouseUp(
-                        button=event['button'], x=event['position'][0], y=event['position'][1])
+                if last_time and not self.god_speed:
+                    time.sleep(event['time'] - last_time)
                 else:
-                    pag.mouseDown(
-                        button=event['button'], x=event['position'][0], y=event['position'][1])
-            elif event['event_name'] == 'WheelEvent':
-                if event['event_type'] == 'up':
-                    pag.scroll(1, x=event['position'][0],
-                               y=event['position'][1])
+                    time.sleep(0.05)
+                last_time = event['time']
+
+                # Action
+                if event['event_name'] == 'KeyboardEvent':
+                    if event['key']['char']:
+                        key = event['key']['char']
+                    else:
+                        if platform.system() == 'Linux':
+                            key = Key(LinuxKeyCode._from_symbol(
+                                event['key']['_symbol']))
+                        elif platform.system() == 'Darwin':
+                            # To do
+                            pass
+                        else:
+                            # To do
+                            pass
+
+                    if event['event_type'] == 'up':
+                        kc.release(key)
+                    else:
+                        kc.press(key)
                 else:
-                    pag.scroll(-1, x=event['position']
-                               [0], y=event['position'][1])
-            else:
-                if platform.system() == 'Windows':
-                    key = event['name']
-                else:
-                    key = event['scan_code'] or event['name']
+                    mc.position = event['position']
+                    if event['event_name'] in ['TouchEvent', 'ClickEvent']:
+                        btn = Button(event['button']['_value_']
+                                     ) if 'button' in event else Button.left
 
-                if event['event_type'] == 'up':
-                    kb.release(key)
-                else:
-                    kb.press(key)
+                        if event['event_type'] == 'up':
+                            mc.release(btn)
+                        else:
+                            mc.press(btn)
+                    elif event['event_name'] == 'WheelEvent':
+                        if event['event_type'] == 'up':
+                            mc.scroll(0, 1)
+                        else:
+                            mc.scroll(0, -1)
 
-        kb.restore_modifiers(state)
+            logger.info('Replay finished')
+            s.call(['notify-send', 'Chrono', 'Replay finished.'])
+            self.is_playing = False
 
-        logger.info('Replay finished')
+    def on_press(self, key):
+        logger.info('Pressed {0}'.format(key))
 
-    def kb_event_handler(self, raw_kb_events):
-        for e in raw_kb_events:
-            kb_event_dict = {}
-            kb_event_dict['event_name'] = 'KeyboardEvent'
-            kb_event_dict.update(json.loads(e.to_json()))
-            self.kb_events.append(kb_event_dict)
+        self.kb_event_handler(key, 'down', time.time())
 
-    def m_event_handler(self, raw_m_event):
+    def on_release(self, key):
+        logger.info('Released {0}'.format(key))
+
+        self.kb_event_handler(key, 'up', time.time())
+        if key == self.escape_key:
+            self.kl.stop()
+            self.ml.stop()
+
+    def on_touch(self, x, y):
+        if self.touch_mode:
+            logger.info('Touched at {0}'.format((x, y)))
+
+            self.m_event_handler('down', (x, y), time.time())
+            self.m_event_handler('up', (x, y), time.time())
+
+    def on_click(self, x, y, button, pressed):
+        logger.info('{0} {1} at {2}'.format(
+            'Pressed' if pressed else 'Released', button, (x, y)))
+
+        if self.touch_mode:
+            self.touch_mode = False
+
+        event_type = 'down' if pressed else 'up'
+        self.m_event_handler(event_type, (x, y), time.time(), button)
+
+    def on_scroll(self, x, y, dx, dy):
+        event_type = 'down' if dy < 0 else 'up'
+        logger.info('Scrolled {0} at {1}'.format(event_type, (x, y)))
+
+        if self.touch_mode:
+            self.touch_mode = False
+
+        self.m_event_handler(event_type, (x, y), time.time())
+
+    def kb_event_handler(self, key, event_type, time):
+        kb_event_dict = {}
+
+        kb_event_dict['event_name'] = 'KeyboardEvent'
+        kb_event_dict['event_type'] = event_type
+        kb_event_dict['key'] = vars(
+            vars(key)['_value_']) if '_value_' in vars(key) else vars(key)
+        kb_event_dict['time'] = time
+
+        self.kb_events.append(kb_event_dict)
+
+    def m_event_handler(self, event_type, position, time, button=None):
         m_event_dict = {}
 
-        if isinstance(raw_m_event, m.ButtonEvent) and raw_m_event.button in ['?', 'left', 'middle', 'right']:
-            m_event_dict['event_name'] = 'ButtonEvent'
-            m_event_dict['event_type'] = raw_m_event.event_type
-            m_event_dict['button'] = 'left' if raw_m_event.button == '?' else raw_m_event.button
-        elif isinstance(raw_m_event, m.WheelEvent):
-            m_event_dict['event_name'] = 'WheelEvent'
-            m_event_dict['event_type'] = 'up' if raw_m_event.delta > 0 else 'down'
+        if self.touch_mode:
+            m_event_dict['event_name'] = 'TouchEvent'
+        else:
+            if button:
+                m_event_dict['event_name'] = 'ClickEvent'
+                btn = vars(button)
+                btn.pop('__objclass__', None)
+                m_event_dict['button'] = btn
+            else:
+                m_event_dict['event_name'] = 'WheelEvent'
 
-        if 'event_name' in m_event_dict:
-            m_event_dict['position'] = m.get_position()
-            m_event_dict['time'] = raw_m_event.time
-            self.m_events.append(m_event_dict)
+        m_event_dict['event_type'] = event_type
+        m_event_dict['position'] = position
+        m_event_dict['time'] = time
+
+        self.m_events.append(m_event_dict)
 
     def save(self):
         events = sorted(self.kb_events + self.m_events,
                         key=lambda i: i['time'])
-
-        # Fix escape_key pressing issue
-        events.append({
-            'event_name': 'KeyboardEvent',
-            'event_type': 'up',
-            'scan_code': events[-1]['scan_code'],
-            'name': events[-1]['name'],
-            'time': events[-1]['time'] + 0.1
-        })
 
         # To do: dynamic file name
         with open('events.json', 'w') as f:
@@ -127,12 +191,8 @@ class Api:
 
         return events
 
-    def thread_handler(self):
-        kb.unhook(kb.hook(set().add))
-        kb.release(self.escape_key)
-
 
 if __name__ == '__main__':
     api = Api()
     window = webview.create_window('Chrono', 'assets/index.html', js_api=api)
-    webview.start(api.thread_handler)
+    webview.start()
