ec23094024ffc6e875c932df5e4f287e86d5abb8
Tunghim Lam <tunghim@cronumax.com>
2021-09-15 23:52:12 +0800

    v1.1.6

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ class Api:
     def __init__(self):
         logger.info('Chrono started')
 
-        self.version = '1.1.5'
+        self.version = '1.1.6'
         self.host = platform.node()
         self.host_os = platform.system()
         self.host_username = getpass.getuser()
