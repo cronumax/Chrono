bc05222c77cd291619bc4a657e1f995713fe6662
Tunghim Lam <tunghim@cronumax.com>
2022-02-09 11:21:31 +0800

    v1.2.7

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ class Api:
     def __init__(self):
         logger.info('Chrono started')
 
-        self.version = '1.2.6'
+        self.version = '1.2.7'
         self.host = platform.node()
         self.host_os = platform.system()
         self.host_username = getpass.getuser()
